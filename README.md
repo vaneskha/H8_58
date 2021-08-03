214b2010518428f1402a8ea5432f451427ff7fb0
vaneskha <vanaenas@gmail.com>
2021-08-03 21:30:49 +0700

    add Sesi 3 & README

---
+++ README.md
@@ -0,0 +1,6 @@
+<h3> PYTN Data Science
+<h4> Batch 58 | Vaneskha Frischilya
+<p> Contents:
+Sesi 1
+Sesi 2
+Sesi 3
---
+++ Sesi3/mod.py
@@ -0,0 +1,8 @@
+s = "Hacktiv8-PTP Python For Data Science"
+a = [100, 200, 300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
